621d689673ccb543c16176a59fa2750da7a6bee9
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-08-11 00:14:37 +0200

    Add BaseController with IMediatr.

---
+++ Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Api/Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Modules.Inventory.Api.Controllers
+{
+    [ApiController]
+    [Route("api/" + InventoryModule.BasePath + "/[controller]")]
+    internal abstract class BaseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        protected ActionResult<T> OkOrNotFound<T>(T model)
+        {
+            if (model is not null)
+            {
+                return Ok(model);
+            }
+
+            return NotFound();
+        }
+    }
+}
