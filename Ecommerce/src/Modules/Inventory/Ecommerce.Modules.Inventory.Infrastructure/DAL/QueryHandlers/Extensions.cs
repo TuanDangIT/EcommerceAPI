79a506ef33f905ecad5be870d8bde2e4b744be20
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-08-11 20:30:16 +0200

    Add AsDto extension file and change Id to diffrent name.

---
+++ Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Infrastructure/DAL/QueryHandlers/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Modules.Inventory.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Modules.Inventory.Infrastructure.DAL.QueryHandlers
+{
+    internal static class Extensions
+    {
+        public static ManufacturerDto AsDto(this ManufacturerDto manufacturer)
+        {
+
+        }
+    }
+}
