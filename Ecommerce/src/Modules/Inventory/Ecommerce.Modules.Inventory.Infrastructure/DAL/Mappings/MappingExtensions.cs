1c1d277867ced1d7f02d3c1b153df6961ca8f3e9
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-08-11 20:37:50 +0200

    Delete extensions file and add MappingExtensions.

---
+++ Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Infrastructure/DAL/Mappings/MappingExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Modules.Inventory.Application.DTO;
+using Ecommerce.Modules.Inventory.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Modules.Inventory.Infrastructure.DAL.Mappings
+{
+    internal static class MappingExtensions
+    {
+        public static ManufacturerDto AsDto(this Manufacturer manufacturer)
+        {
+
+        }
+        public static ParameterDto AsDto(this Parameter parameter)
+        {
+
+        }
+        public static ProductDto AsDto(this Product product)
+        {
+
+        }
+    }
+}
