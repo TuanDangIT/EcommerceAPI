ea776b29bd84e6bdc7d657795a54a9a9c4021e38
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-08-11 21:16:32 +0200

    Add GetManufacturerHandler implementation.

--- Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Infrastructure/DAL/QueryHandlers/GetManufacturerHandler.cs
+++ Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Infrastructure/DAL/QueryHandlers/GetManufacturerHandler.cs
@@ -1,19 +1,28 @@
 ﻿using Ecommerce.Modules.Inventory.Application.DTO;
 using Ecommerce.Modules.Inventory.Application.Features.Manufacturers.GetManufacturer;
 using Ecommerce.Shared.Abstractions.MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ecommerce.Modules.Inventory.Infrastructure.DAL.Mappings;
 
 namespace Ecommerce.Modules.Inventory.Infrastructure.DAL.QueryHandlers
 {
-    internal sealed class GetManufacturerHandler : IQueryHandler<GetManufacturer, ManufacturerDto>
+    internal sealed class GetManufacturerHandler : IQueryHandler<GetManufacturer, ManufacturerDto?>
     {
-        public Task<ManufacturerDto> Handle(GetManufacturer request, CancellationToken cancellationToken)
+        private readonly InventoryDbContext _dbContext;
+
+        public GetManufacturerHandler(InventoryDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+        }
+        public async Task<ManufacturerDto?> Handle(GetManufacturer request, CancellationToken cancellationToken)
+        {
+            var manufacturer = await _dbContext.Manufacturers.AsNoTracking().SingleOrDefaultAsync(m => m.Id == request.ManufacturerId);
+            return manufacturer?.AsDto();
         }
     }
 }
