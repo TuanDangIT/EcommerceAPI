e21a4347c7979d5e16a31e2334b47606ae2694be
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-08-26 00:32:20 +0200

    Add standard implementation of controllers.

---
+++ Ecommerce/src/Modules/Products/Ecommerce.Modules.Products.Api/Controllers/ReviewController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Modules.Products.Api.Controllers
+{
+    internal class ReviewController : BaseController
+    {
+    }
+}
