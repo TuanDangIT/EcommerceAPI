366b54550b37fd7e39d1f112e7ce18ec942fbf63
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-09-07 21:13:12 +0200

    Drop some private modifiers.

--- Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Domain/Inventory/Entities/ProductParameter.cs
+++ Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Domain/Inventory/Entities/ProductParameter.cs
@@ -11,10 +11,10 @@ namespace Ecommerce.Modules.Inventory.Domain.Inventory.Entities
         public int Id { get; private set; }
         public Product Product { get; private set; } = new();
         public Guid ProductId { get; private set; }
-        public Parameter Parameter { get; private set; } = new();
+        public Parameter Parameter { get; set; } = new();
         public Guid ParameterId { get; private set; }
-        public string Value { get; private set; } = string.Empty;
-        public DateTime CreatedAt { get; private set; }
+        public string Value { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; private set; }
     }
 }
