a371e017176a9a5e77f8ea96215f8dbf8d7abe6c
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2025-03-29 00:33:35 +0100

    Add CustomerId to review migration.

---
+++ Ecommerce/src/Modules/Inventory/Ecommerce.Modules.Inventory.Infrastructure/DAL/Migrations/20250328232945_AddCustomerIdToReview.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Modules.Inventory.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerIdToReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerId",
+                schema: "inventory",
+                table: "Reviews",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                schema: "inventory",
+                table: "Reviews");
+        }
+    }
+}
